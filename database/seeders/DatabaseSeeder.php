5b70c334c918ac02a631e343064fc85e9a36a361
RizkyaSalsabila <rzkysalsabila@gmail.com>
2026-01-20 07:39:30 +0700

    menambahkan database dan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(PenggunaSeeder::class);
+        $this->call(SuratMasukSeeder::class);
+        $this->call(SuratKeluarSeeder::class);
     }
 }
